91d597869431baf508a504e840338d3f29bfdd01
smyeungx <42128034+smyeungx@users.noreply.github.com>
2021-01-22 23:59:30 +0800

    Update setssdroot.sh

--- data/setssdroot.sh
+++ data/setssdroot.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# Runs at startup, switches rootfs to the SSD on nvme0 (M.2 Key M slot)
+#Runs at startup, switches rootfs to the SSD on nvme0 (M.2 Key M slot)
 NVME_DRIVE="/dev/nvme0n1p1"
 CHROOT_PATH="/nvmeroot"
 
 INITBIN=/lib/systemd/systemd
-EXT4_OPT="-o defaults -o errors=remount-ro -o discard"
+EXT4_OPT="-o defaults -o debug -o errors=continue -o discard"
+
+echo "setssdroot: mount and switch rootfs to nvme0n1p1" | tee /dev/kmsg
 
 modprobe ext4
 
@@ -13,3 +15,5 @@ mount -t ext4 ${EXT4_OPT} ${NVME_DRIVE} ${CHROOT_PATH}
 
 cd ${CHROOT_PATH}
 /bin/systemctl --no-block switch-root ${CHROOT_PATH}
+
+echo "setssdroot: exit mount and switch rootfs" | tee /dev/kmsg
