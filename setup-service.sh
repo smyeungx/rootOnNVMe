1adb9e4facc61f285ce99d991ab2f73070321961
smyeungx <42128034+smyeungx@users.noreply.github.com>
2021-02-13 16:09:46 +0800

    Update setup-service.sh

--- setup-service.sh
+++ setup-service.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 # Setup the service to set the rootfs to point to the SSD
 sudo cp data/setssdroot.service /etc/systemd/system
+sudo cp data/reset-usb-root-devices.service /etc/systemd/system
 sudo cp data/setssdroot.sh /sbin
+sudo cp data/reset-usb-root-devices.sh /sbin
 sudo chmod 777 /sbin/setssdroot.sh
+sudo chmod 777 /sbin/reset-usb-root-devices.sh
 systemctl daemon-reload
 sudo systemctl enable setssdroot.service
+sudo systemctl enable reset-usb-root-devices.service
 
 # Copy these over to the SSD
 sudo cp /etc/systemd/system/setssdroot.service /mnt/etc/systemd/system/setssdroot.service
 sudo cp /sbin/setssdroot.sh /mnt/sbin/setssdroot.sh
+sudo cp /etc/systemd/system/reset-usb-root-devices.service /mnt/etc/systemd/system/reset-usb-root-devices.service
+sudo cp /sbin/reset-usb-root-devices.sh /mnt/sbin/reset-usb-root-devices.sh
 
 # Create setssdroot.conf which tells the service script to set the rootfs to the SSD
 # If you want to boot from SD again, remove the file /etc/setssdroot.conf from the SD card.
